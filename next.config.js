07a5a6bbbceb2ef2ba0bc1b2026526b17032c849
miya264 <tmiyajima117@gmail.com>
2025-06-07 02:40:48 +0900

    next.config修正

--- next.config.js
+++ next.config.js
@@ -1,28 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  output: "standalone",
-  transpilePackages: ['@yudiel/react-qr-scanner'],
-  experimental: {
-    serverActions: true,
-  },
-  webpack: (config) => {
-    if (!config.resolve) {
-      config.resolve = {};
-    }
-    if (!config.resolve.fallback) {
-      config.resolve.fallback = {};
-    }
-    
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      path: false,
-    };
-    
-    return config;
-  },
-};
-
-module.exports = nextConfig; 
+    reactStrictMode: true,
+    output: "standalone",
+    transpilePackages: ['@yudiel/react-qr-scanner'],
+    experimental: {
+      serverActions: true,
+    },
+    compiler: {
+      swcMinify: true, // ← 正しい場所に移動
+    },
+    webpack: (config) => {
+      if (!config.resolve) {
+        config.resolve = {};
+      }
+      if (!config.resolve.fallback) {
+        config.resolve.fallback = {};
+      }
+  
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        path: false,
+      };
+  
+      return config;
+    },
+  };
+  
+  module.exports = nextConfig;
+  
